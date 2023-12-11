074bf19a4c61472d125936b2f9d6ba676970baa0
zahin <mdishmamzahin@gmail.com>
2023-12-11 19:58:43 +0600

    created app

--- simpleProject/settings.py
+++ simpleProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e*foul1!a9t6xx*_numi1kq!vnx2%7*qopbzo@@^@_wq3s+d$i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['zahin420.pythonanywhere.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myWebsite'
 ]
 
 MIDDLEWARE = [
